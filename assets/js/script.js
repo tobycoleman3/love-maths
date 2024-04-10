4d2d6fb153b00fc8ae1f9677ccd30ca1afb4ca96
toby coleman <tobycoleman3@hotmail.co.uk>
2024-04-10 10:11:52 +0000

    made basic functions for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+// wait for the DOM to finish loading before running the game
+// get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("you clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`);
+            }
+        })
+    }
+})
 function runGame() {
 
 }
@@ -31,5 +48,5 @@ function displayMultiplyQuestion() {
 }
 
 function displayDivisionQuestion() {
-    
+
 }
